7f71aa5138034bd130d20003007f5c4d42b0ce34
Adam Driggers <Adam@driggs.local>
2022-02-04 15:45:50 -0500

    menu action

--- script.js
+++ script.js
@@ -1,45 +1,66 @@
+console.log('working');
 
-
-//click listener for the game list
 window.onload = function(){
-  //alert("working");
-  var game = document.getElementsByClassName("game")
-  
-  for(var i=0; i < game.length; i++){
-      game[i].addEventListener('click', update);
-  }
-  
-  
-  
+  //make the first link the selected on by default
+  //get all the links
+  // document.getElemen
+  document.querySelector('.game').classList.add('selected');
+  //add an event listener for the keyboard
+  document.addEventListener("keyup", function(e){
+    console.log('key', e.keyCode, e.key);
+    //select the first list item inside the nav class
+    //add the class of curent
+    //if the key is up
+      //remove the selected class from the current list item that has it
+      //if the first is already selected, move to the last item
+      //move up one list item
+    var links = document.getElementsByClassName('game');
+    var current;
+
+    for(var i = 0; i < links.length; i++){
+      if(links[i].classList.contains('selected')){
+        console.log(i);
+        current = i;
+      } 
+    }
+
+    if(e.keyCode == 38 || e.key == "ArrowUp"){
+      //remove the class on current link
+      links[current].classList.remove('selected');
+      if(current == 0){
+       links[links.length-1].classList.add('selected');
+      } else {
+        links[current - 1].classList.add('selected');
+      }
+    }
+
+    if(e.keyCode == 40 || e.key == "ArrowDown"){
+      //remove the class on current link
+      links[current].classList.remove('selected');
+      if(current == links.length-1){
+       links[0].classList.add('selected');
+      } else {
+        links[current + 1].classList.add('selected');
+      }
+    }
+    //if the key is down
+      //remove the selected class fromt he current list item that has it
+      //if at the last item, move to the first
+      //otherwise move down one list item
+
+    //if the enter key is hti, keyCode 13
+      //get the href of the a that is selected
+      //send the window to that location
+    if(e.keyCode == 65 || e.key == "a"){
+      console.log('enter');
+      var url = links[current].getAttribute('href')
+      window.location.assign(url);
+    }
+
+    if(e.keyCode == 27 || e.key == "Escape"){
+      console.log('escape to reload');
+      location.reload();
+    }
+
+  });
 }
-
-//update game function
-function update(e){
-  console.log(e);
-  var id = e.target.getAttribute('data-game-id');
-  var iframe = document.getElementsByTagName('iframe')[0]
-  var url = "https://scratch.mit.edu/projects/" + id + "/embed/"
-  
-  iframe.setAttribute('src', url);
-
-  
-}
-
-function greenFlag(){
-  var iframe = document.querySelector('iframe');
-  var preview = iframe.contentWindow.document.querySelector('.stage_green-flag-overlay-wrapper_2hUi_')
-  //var preview = document.querySelector('.stage_green-flag-overlay-wrapper_2hUi_')
-  var e = document.createEvent("Events");
-  e.initEvent("click", true, false);
-  //debugger;
-  preview.dispatchEvent(e)
-  
-}
-
-//var iframe = document.getElementById('iframeId');
-//var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-
-//var test = document.querySelector('.stage_green-flag-overlay-wrapper_2hUi_')
-//var e = document.createEvent("Events");
-//e.initEvent("click", true, false);
-//test.dispatchEvent(e)
